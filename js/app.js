7372934be7baaf23cc6cfdccad9e8e211dc34216
Tulio <maqtulio@gmail.com>
2019-09-30 11:47:15 -0300

    more img

--- js/app.js
+++ js/app.js
@@ -2,13 +2,35 @@ var filmes = [
   "20988-cartaz.jpg",
   "22678-cartaz.jpg",
   "22726-cartaz.jpg",
+  "22784-cartaz.jpg",
+  "22786-cartaz.jpg",
   "22805-cartaz.jpg",
   "22885-cartaz.jpg",
   "22947-cartaz.jpg",
   "23004-cartaz.jpg"
 ];
-var eventos = ["22950-cartaz.jpg", "22999-cartaz.jpg"];
-var shows = ["22895-cartaz.jpg"];
+var eventos = [
+  "22049-cartaz.jpg",
+  "22050-cartaz.jpg",
+  "22423-cartaz.jpg",
+  "22583-cartaz.jpg",
+  "22633-cartaz.jpg",
+  "22794-cartaz.jpg",
+  "22919-cartaz.jpg",
+  "22950-cartaz.jpg",
+  "22999-cartaz.jpg"
+];
+var shows = [
+  "22747-cartaz.jpg",
+  "22749-cartaz.jpg",
+  "22757-cartaz.jpg",
+  "22775-cartaz.jpg",
+  "22776-cartaz.jpg",
+  "22778-cartaz.jpg",
+  "22779-cartaz.jpg",
+  "22780-cartaz.jpg",
+  "22895-cartaz.jpg"
+];
 
 class card {
   constructor(src, tipo) {
